17ea6a481b05ed6025918ba1e6d833cd41ad3ad2
betrakiss <angelin.nedelchev@outlook.com>
2017-04-15 15:29:07 +0300

    Add Web API project.

---
+++ src/HomeSweetHome/Server/Controllers/HeartbeatController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Server.Controllers
+{
+    [Route("api/[controller]")]
+    public class HeartbeatController : Controller
+    {
+        // GET: api/values
+        [HttpGet]
+        public IActionResult Heartbeat()
+        {
+            return Ok("Kewl");
+        }
+
+        [HttpGet]
+        [Route("init")]
+        public IActionResult Initialize()
+        {
+            return Ok("That's oke");
+        }
+    }
+}
