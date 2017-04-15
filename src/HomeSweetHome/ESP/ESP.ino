b126b2c7f0b698cbcf8974cd5b11064099e71a4d
betrakiss <angelin.nedelchev@outlook.com>
2017-04-16 00:57:03 +0300

    Change heartbeat to once every 5 minutes.

--- src/HomeSweetHome/ESP/ESP.ino
+++ src/HomeSweetHome/ESP/ESP.ino
@@ -5,22 +5,22 @@
 
 #include <ESP8266HTTPClient.h>
 
-#define USE_SERIAL Serial
-
-#define HEARTBEAT 100
-#define OK 200
-#define ERROR 500
+#define KEY_BEGIN 0
+#define KEY_END 2
+#define VALUE_BEGIN 3
+#define DELIMITER '\t'
+#define HEARTBEAT_INTERVAL 300000
 
 ESP8266WiFiMulti WiFiMulti;
+
+time_t lastHeartbeat = millis();
 
 void setup()
 {
 	Serial.begin(9600);
-	Serial.println();
-	Serial.println();
-	Serial.println();
 
-	for (uint8_t t = 4; t > 0; t--) {
+	for (uint8_t t = 4; t > 0; t--) 
+	{
 		Serial.printf("[SETUP] WAIT %d...\n", t);
 		Serial.flush();
 		delay(1000);
@@ -28,22 +28,34 @@ void setup()
 
 	WiFiMulti.addAP("The Pepper", "9582757280");
 
-
-	sendRequest("http://192.168.0.104:55155/api/init");
+	sendRequest("http://192.168.0.104:55155/api/events?eventType=init");
 	Serial.println("Hello. I'm ESP8266. Nice to collaborate with you.\t");
 }
 
 void processCommand(const String& command)
 {
+	String key = command.substring(KEY_BEGIN, KEY_END);
+	String value = command.substring(VALUE_BEGIN);
+
+	if (key == "TP")
+	{
+
+	}
+
 	Serial.write("OK MOTHERFUCKER\t");
 }
 
 void loop()
 {
 	while (Serial.available())
-		processCommand(Serial.readStringUntil('\t'));
+		processCommand(Serial.readStringUntil(DELIMITER));
 
-	heartbeat();
+	if (millis() - lastHeartbeat > HEARTBEAT_INTERVAL)
+	{
+		heartbeat();
+		lastHeartbeat = millis();
+	}
+
 	delay(3000);
 }
 
@@ -53,35 +65,29 @@ void heartbeat()
 	Serial.write("HEARTBEAT\t");
 }
 
-void sendRequest(String address) {
-	// wait for WiFi connection
+void sendRequest(String address) 
+{
 	if ((WiFiMulti.run() == WL_CONNECTED)) 
 	{
 		HTTPClient http;
 
-		Serial.print("[HTTP] begin...\n");
-		// configure traged server and url
-		//http.begin("https://192.168.1.12/test.html", "7a 9c f4 db 40 d3 62 5a 6e 21 bc 5c cc 66 c8 3e a1 45 59 38"); //HTTPS
-		http.begin(address); //HTTP
+		http.begin(address);
 
-		Serial.print("[HTTP] GET...\n");
-		// start connection and send HTTP header
 		int httpCode = http.GET();
 
 		// httpCode will be negative on error
-		if (httpCode > 0) {
-			// HTTP header has been send and Server response header has been handled
+		if (httpCode > 0) 
+		{
 			Serial.printf("[HTTP] GET... code: %d\n", httpCode);
 
-			// file found at server
-			if (httpCode == HTTP_CODE_OK) {
+			if (httpCode == HTTP_CODE_OK) 
+			{
 				String payload = http.getString();
 				Serial.println(payload);
 			}
 		}
-		else {
+		else
 			Serial.printf("[HTTP] GET... failed, error: %s\n", http.errorToString(httpCode).c_str());
-		}
 
 		http.end();
 	}
