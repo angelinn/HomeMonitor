d9a0878390e4d96fe982488f7cc9d28ae31ec05d
betrakiss <angelin.nedelchev@outlook.com>
2017-04-17 21:41:50 +0300

    Add Measurement entity.

---
+++ src/HomeSweetHome/ServiceLayer/DataAccess/Entities/Measurement.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ServiceLayer.DataAccess.Entities
+{
+    public class Measurement
+    {
+        public int MeasurementId { get; set; }
+        public string Sensor { get; set; }
+        public double Value { get; set; }
+        public DateTime Time { get; set; }
+    }
+}
