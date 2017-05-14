cc686d9aef9dc631b6b5bd949db997663135a7d6
betrakiss <angelin.nedelchev@outlook.com>
2017-05-14 22:42:14 +0300

    Update controller for new tables.

--- src/HomeSweetHome/Server/Controllers/EventsController.cs
+++ src/HomeSweetHome/Server/Controllers/EventsController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using ServiceLayer.DataAccess;
 using ServiceLayer.DataAccess.Entities;
+using ServiceLayer.DataAccess.Enums;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -21,17 +22,45 @@ namespace Server.Controllers
         }
 
         [HttpGet]
-        public async Task<IActionResult> Event(string eventType)
+        [Route("Weather")]
+        public async Task<IActionResult> Weather(string temperature, string humidity)
         {
-            //await context.Events.AddAsync(new Event
-            //{
-            //    Address = HttpContext.Connection.RemoteIpAddress.ToString(),
-            //    EventText = eventType,
-            //    Time = DateTime.Now
-            //});
+            await context.Weather.AddAsync(new Weather
+            {
+                Temperature = Int32.Parse(temperature),
+                Humidity = Int32.Parse(humidity),
+                Timestamp = DateTime.Now
+            });
 
             await context.SaveChangesAsync();
 
+            return Ok();
+        }
+
+        [HttpGet] 
+        [Route("Door")]
+        public async Task<IActionResult> Door(int status)
+        {
+            await context.Door.AddAsync(new Door
+            {
+                Status = (DoorStatus)status,
+                Timestamp = DateTime.Now
+            });
+
+            await context.SaveChangesAsync();
+            return Ok();
+        }
+
+        [HttpGet]
+        [Route("Movement")]
+        public async Task<IActionResult> Movement()
+        {
+            await context.Movement.AddAsync(new Movement
+            {
+                Timestamp = DateTime.Now
+            });
+
+            await context.SaveChangesAsync();
             return Ok();
         }
     }
